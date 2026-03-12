91dc3ebabce206cf481ebfb1afae6e7678e144e1
Deyvidhalaten <108770440+Deyvidhalaten@users.noreply.github.com>
2026-03-12 14:52:53 -0300

    19-{Feature}AutenticacaoApiBistek

--- app/controllers/auth_controller.py
+++ app/controllers/auth_controller.py
@@ -1,10 +1,12 @@
+import asyncio
+
 from flask import Blueprint, request, jsonify
-from app.services.auth_service import check_login, generate_auth_token
+from app.services.auth_service import check_login, generate_auth_token, realizar_login_ad
 
 bp = Blueprint("auth_controller", __name__, url_prefix="/api")
 
 @bp.route("/login", methods=["POST", "OPTIONS"])
-def login():
+async def login():
     if request.method == "OPTIONS":
         return "", 204
     
@@ -12,8 +14,21 @@ def login():
     user = data.get("username", "").strip()
     pwd = data.get("password", "").strip()
     
-    if check_login(user, pwd):
+    autenticado = await realizar_login_ad(user, pwd)
+    print(autenticado)
+
+    if autenticado.sucesso:
+
+        from app.services.auth_service import generate_auth_token
         token = generate_auth_token(user)
-        return jsonify({"success": True, "token": token})
+        return jsonify({
+            "success": True, 
+            "token": token,
+            "mensagem": autenticado.mensagem
+        })
     
-    return jsonify({"success": False, "error": "Credenciais inválidas"}), 401
+
+    return jsonify({
+        "success": False, 
+        "error": str(autenticado.mensagem) 
+    }), 401
--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,9 +1,15 @@
+from base64 import urlsafe_b64decode, urlsafe_b64encode
 import json
 from pathlib import Path
+import platform
+from typing import Any
+from aiohttp import ClientSession
+from pydantic import BaseModel
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import URLSafeTimedSerializer, SignatureExpired, BadSignature
 from flask import current_app, request, jsonify
 from functools import wraps
+from cryptography.fernet import Fernet 
 
 def get_users_file() -> Path:
     return current_app.config["DIRS"]["data"] / "users.json"
@@ -15,7 +21,7 @@ def init_users_file(data_dir: Path):
         # default user: admin, senha: 123
         # Mudar isso na UI mais tarde
         default_user = {
-            "admin": generate_password_hash("123")
+            "deyvid.silva": "2"
         }
         with users_file.open("w", encoding="utf-8") as f:
             json.dump(default_user, f, indent=4)
@@ -65,3 +71,68 @@ def require_auth(f):
             
         return f(*args, **kwargs)
     return decorated
+
+
+BAPI = "https://api.bistek.com.br" # environ["BSTK_BAPI"]
+TOKEN = "eyJ0eXAiOiAiSldUIiwgImFsZyI6ICJIUzI1NiJ9.eyJ1c3VhcmlvIjogImRleXZpZC5zaWx2YSIsICJzZW5oYSI6ICI1Rm4wTipOUCIsICJ0aW1lb3V0IjogMjA4ODY4MTcxMi44Mjk2NTR9.nSqkYEARLEVhKxmnNIPWnoskS-FwN5JqkCYTIAN-Nlk=" # environ["TOKEN_AD"]
+HEADERS = {
+    "Authorization": f"Bearer {TOKEN}",
+    "Content-Type": "application/json",
+}
+
+class Autenticado(BaseModel):
+    sucesso: bool = False
+    mensagem: str = "Nenhuma"
+    status: int = 401
+
+
+async def patch_bapi_autenticador(envio: dict) -> Autenticado:
+    if not TOKEN:
+        return Autenticado(mensagem="Token não informado")
+
+    async with ClientSession(headers=HEADERS) as s:
+        async with s.patch(f"{BAPI}/ad/autenticacao", json=envio, ssl=False) as r:
+            retorno = {"ok": False}
+            if r.status in(200, 400, 401):
+                retorno = await r.json()
+            return Autenticado(
+                sucesso=retorno.get("ok") or False,
+                mensagem=str(retorno.get("msg") or r.reason),
+                status=r.status,
+            )
+
+
+def decode(d: Any = None) -> str:
+    return urlsafe_b64encode(d or Fernet.generate_key()).decode("UTF-8")
+
+
+def empacota(segredo: str, dado: dict) -> str:
+    return decode(
+        urlsafe_b64decode(
+            Fernet(urlsafe_b64decode(segredo)).encrypt(
+                json.dumps(dado).encode()
+            )
+        )
+    )
+
+
+async def realizar_login_ad(usuario: str, senha: str) -> Autenticado:
+    if not usuario:
+        return Autenticado(mensagem="Usuário não informado")
+    if not senha:
+        return Autenticado(mensagem="Senha não informada")
+
+    try:
+        segredo = [decode() for _ in range(2)]
+        password = empacota(segredo[1], {"senha": senha})
+        dado = empacota(segredo[0], {
+            "login":usuario, "token": segredo[1], "pass": password
+        })
+        envio = {"data": dado, "secret": segredo[0]}
+        return await patch_bapi_autenticador(envio)
+    except Exception as e:
+        return Autenticado(mensagem=str(e))
+
+
+def isWindows() -> bool:
+    return platform.system() == "Windows"
